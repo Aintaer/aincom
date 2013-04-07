02d1e72a8ed6a9c15550a1e9650c83e888615413
Alex Lee <aintaer@aintaer.com>
2013-04-07 13:14:57 -0400

    Static routes in place for app

--- app.js
+++ app.js
@@ -2,12 +2,36 @@ var flatiron = require('flatiron'),
     path = require('path'),
     app = flatiron.app;
 
-app.config.file({ file: path.join(__dirname, 'config', 'config.json') });
+/**
+ * Application configuration
+ */
 
-app.use(flatiron.plugins.http);
-
-app.router.get('/', function () {
-  this.res.json({ 'hello': 'world' })
+// First look at conf file
+app.config.file({ 
+	file: path.join(__dirname, 'config', 'config.json')
+});
+// Otherwise, defaults here
+app.config.defaults({
+	root: path.join(__dirname, 'public')
 });
 
-app.start(3000);
+/**
+ * Middleware
+ */
+app.use(flatiron.plugins.http, {
+	// Union routes
+	before: [require('./lib/static')],
+	after: []
+});
+
+/**
+ * Routes
+ */
+app.router.get('/', function () {
+  this.res.json({ 'oh': 'noes!' });
+});
+
+// Kickoff
+app.start(8080, function() {
+	console.log("Started!");
+});
