1c0cfc10deb98db2a3878fe0e8ee34e61d0ef1ed
Alex Lee <aintaer@aintaer.com>
2014-06-28 17:45:06 +0000

    app/index: splitting into bootstrap file

--- contents/js/app/index.js
+++ contents/js/app/index.js
@@ -1,19 +1,4 @@
-var html = document.documentElement,
-config = {
-	kitId: 'dtz5muo',
-	scriptTimeout: 3000
-};
-
-// Typekit
-html.classList.add('wf-loading');
-require([`//use.typekit.net/${config.kitId}.js`], () => {
-	try {
-		Typekit.load(config);
-	} catch (tke) {}
-}, () => {
-	html.classList.remove('wf-loading');
-	html.classList.add('wf-inactive')
-});
+import "app/bootstrap";
 
 document.querySelectorAll('nav li')
 .forEach(li => li.addEventListener('click', function() {
