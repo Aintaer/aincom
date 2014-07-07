ad58a8d27aaa4effa1f303930c2ca7d5fa5e4302
Alex Lee <aintaer@aintaer.com>
2014-07-07 02:08:24 +0000

    Nav switching now does fancy underlining

--- contents/js/app/index.js
+++ contents/js/app/index.js
@@ -1,4 +1,18 @@
+var nav = document.querySelector('nav'),
+underscore = document.createElement('div');
+underscore.className = "underscore";
+
+var positionUnderscore = function() {
+	var left = this.offsetLeft;
+	var width = this.offsetWidth;
+	underscore.style.left = `${left}px`;
+	underscore.style.width = `${width}px`;
+};
+
 document.querySelectorAll('nav li')
-.forEach(li => li.addEventListener('click', function() {
-	document.body.className = this.className;
-}));
+.forEach(li => li.addEventListener('click', positionUnderscore));
+
+import async from 'nbd/util/async';
+
+nav.appendChild(underscore);
+async(() => positionUnderscore.call(nav.querySelector('.ident')));
